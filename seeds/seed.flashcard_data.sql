cbabeb61a344763339766d8a545522cc186a7e09
Jim <jsmith774@gmail.com>
2020-09-07 12:40:27 -0400

    updated db seed data

--- seeds/seed.flashcard_data.sql
+++ seeds/seed.flashcard_data.sql
@@ -7,16 +7,16 @@ TRUNCATE
 
 INSERT INTO flashcard(card_prompt, card_answer)
 VALUES
- ('go','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('bunny','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go2','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go3','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go4','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go5','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go6','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go7','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go8','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing'),
- ('go9','https://drive.google.com/file/d/195OfhMPFqmhRrPQLacmQsuXvLV2Tg_nj/view?usp=sharing');
+ ('go','http://hhvstaffing.com/audio/go.m4a'),
+ ('bunny','http://hhvstaffing.com/audio/bunny.m4a'),
+ ('go2','http://hhvstaffing.com/audio/go.m4a'),
+ ('go3','http://hhvstaffing.com/audio/go.m4a'),
+ ('go4','http://hhvstaffing.com/audio/go.m4a'),
+ ('go5','http://hhvstaffing.com/audio/go.m4a'),
+ ('go6','http://hhvstaffing.com/audio/go.m4a'),
+ ('go7','http://hhvstaffing.com/audio/go.m4a'),
+ ('go8','http://hhvstaffing.com/audio/go.m4a'),
+ ('weirdsoundmp3','http://hhvstaffing.com/audio/here_you_go_lighter.mp3');
 
 
 INSERT INTO deck(deck_name) 
